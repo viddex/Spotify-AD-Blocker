14bc57f86c7cc734748e8aefa73700bd9c8311c3
viddex <tacovidde@gmail.com>
2022-01-13 18:20:57 +0100

    Add files via upload

---
+++ Spotify AdBlock.bat
@@ -0,0 +1,179 @@
+@echo off
+cd /
+cd Windows
+cd System32
+cd drivers
+cd etc                                                              
+echo.
+echo.
+echo.
+timeout /t 3
+echo.
+echo.
+echo            ----- PLEASE MAKE SURE YOU RAN CMD AS **ADMINISTRATOR** OR THIS WILL NOT FUNCTION PROPERLY! -----
+echo            ----- PLEASE MAKE SURE YOU RAN CMD AS **ADMINISTRATOR** OR THIS WILL NOT FUNCTION PROPERLY! -----
+echo            ----- PLEASE MAKE SURE YOU RAN CMD AS **ADMINISTRATOR** OR THIS WILL NOT FUNCTION PROPERLY! -----
+echo            ----- PLEASE MAKE SURE YOU RAN CMD AS **ADMINISTRATOR** OR THIS WILL NOT FUNCTION PROPERLY! -----
+echo.
+echo.
+PAUSE
+echo.
+echo.
+echo            Once you have made sure, please continue.
+PAUSE
+timeout /t 1
+echo.
+echo.
+echo  ________  ________  ________   _________  ___  ________   ___  ___  ___  ________   ________                 
+echo ^|\   ____\^|\   __  \^|\   ___  \^|\___   ___\\  \^|\   ___  \^|\  \^|\  \^|\  \^|\   ___  \^|\   ____\                
+echo \ \  \___^|\ \  \^|\  \ \  \\ \  \^|___ \  \_\ \  \ \  \\ \  \ \  \\\  \ \  \ \  \\ \  \ \  \___^|                
+echo  \ \  \    \ \  \\\  \ \  \\ \  \   \ \  \ \ \  \ \  \\ \  \ \  \\\  \ \  \ \  \\ \  \ \  \  ___              
+echo   \ \  \____\ \  \\\  \ \  \\ \  \   \ \  \ \ \  \ \  \\ \  \ \  \\\  \ \  \ \  \\ \  \ \  \^|\  \ ___ ___ ___ 
+echo    \ \_______\ \_______\ \__\\ \__\   \ \__\ \ \__\ \__\\ \__\ \_______\ \__\ \__\\ \__\ \_______\\__\\__\\__\
+echo     \^|_______^|\^|_______^|\^|__^| \^|__^|    \^|__^|  \^|__^|\^|__^| \^|__^|\^|_______^|\^|__^|\^|__^| \^|__^|\^|_______\^|__\^|__\^|__^|
+timeout /t 2
+:: Continuing, inputting the ENTIRE hosts file here. This is gonna take a while to code...
+echo # Copyright (c) 1993-2009 Microsoft Corp. > hosts
+echo # >> hosts
+echo # This is a sample HOSTS file used by Microsoft TCP/IP for Windows. >> hosts
+echo # >> hosts
+echo # This file contains the mappings of IP addresses to host names. Each >> hosts
+echo # entry should be kept on an individual line. The IP address should >> hosts
+echo # be placed in the first column followed by the corresponding host name. >> hosts
+echo # The IP address and the host name should be separated by at least one >> hosts
+echo # space. >> hosts
+echo # >> hosts
+echo # Additionally, comments (such as these) may be inserted on individual >> hosts
+echo # lines or following the machine name denoted by a '#' symbol. >> hosts
+echo # >> hosts
+echo # For example: >> hosts
+echo # >> hosts
+echo #      102.54.94.97     rhino.acme.com          # source server >> hosts
+echo #       38.25.63.10     x.acme.com              # x client host >> hosts
+echo # >> hosts
+echo # localhost name resolution is handled within DNS itself. >> hosts
+echo #	127.0.0.1       localhost >> hosts
+echo #	::1             localhost >> hosts
+echo. >> hosts
+echo 0.0.0.0 adclick.g.doublecklick.net >> hosts
+echo adclick.g.doublecklick.net
+echo. >> hosts
+echo 0.0.0.0 adeventtracker.spotify.com >> hosts
+echo adeventtracker.spotify.com
+echo. >> hosts
+echo 0.0.0.0 ads-fa.spotify.com >> hosts
+echo ads-fa.spotify.com
+echo. >> hosts
+echo 0.0.0.0 analytics.spotify.com >> hosts
+echo analytics.spotify.com
+echo. >> hosts
+echo 0.0.0.0 audio2.spotify.com >> hosts
+echo audio2.spotify.com
+echo. >> hosts
+echo 0.0.0.0 b.scorecardresearch.com >> hosts
+echo b.scorecardresearch.com
+echo. >> hosts
+echo 0.0.0.0 bounceexchange.com >> hosts
+echo bounceexchange.com
+echo. >> hosts
+echo 0.0.0.0 bs.serving-sys.com >> hosts
+echo bs.serving-sys.com
+echo. >> hosts
+echo 0.0.0.0 content.bitsontherun.com >> hosts
+echo content.bitsontherun.com
+echo. >> hosts
+echo 0.0.0.0 core.insightexpressai.com >> hosts
+echo core.insightexpressai.com
+echo. >> hosts
+echo 0.0.0.0 crashdump.spotify.com >> hosts
+echo crashdump.spotify.com
+echo. >> hosts
+echo 0.0.0.0 d2gi7ultltnc2u.cloudfront.net >> hosts
+echo d2gi7ultltnc2u.cloudfront.net
+echo. >> hosts
+echo 0.0.0.0 d3rt1990lpmkn.cloudfront.net >> hosts
+echo d3rt1990lpmkn.cloudfront.net
+echo. >> hosts
+echo 0.0.0.0 desktop.spotify.com >> hosts
+echo desktop.spotify.com
+echo. >> hosts
+echo 0.0.0.0 doubleclick.net >> hosts
+echo doubleclick.net
+echo. >> hosts
+echo 0.0.0.0 ds.serving-sys.com >> hosts
+echo ds.serving-sys.com
+echo.  >> hosts
+echo 0.0.0.0 googleadservices.com >> hosts
+echo googleadservices.com
+echo. >> hosts
+echo 0.0.0.0 googleads.g.doubleclick.net >> hosts
+echo googleads.g.doubleclick.net
+echo. >> hosts
+echo 0.0.0.0 gtssl2-ocsp.geotrust.com >> hosts
+echo gtssl2-ocsp.geotrust.com
+echo. >> hosts
+echo 0.0.0.0 js.moatads.com >> hosts
+echo js.moatads.com
+echo.  >> hosts
+echo 0.0.0.0 log.spotify.com >> hosts
+echo log.spotify.com
+echo. >> hosts
+echo 0.0.0.0 media-match.com >> hosts
+echo media-match.com
+echo. >> hosts
+echo 0.0.0.0 omaze.com >> hosts
+echo omaze.com
+echo.  >> hosts
+echo 0.0.0.0 open.spotify.com >> hosts
+echo open.spotify.com
+echo. >> hosts
+echo 0.0.0.0 pagead46.l.doubleclick.net >> hosts
+echo pagead46.l.doubleclick.net
+echo. >> hosts
+echo 0.0.0.0 pagead2.googlesyndication.com >> hosts
+echo pagead2.googlesyndication.com
+echo. >> hosts
+echo 0.0.0.0 partner.googleadservices.com >> hosts
+echo partner.googleadservices.com
+echo. >> hosts
+echo 0.0.0.0 pubads.g.doubleclick.net >> hosts
+echo pubads.g.doubleclick.net
+echo. >> hosts
+echo 0.0.0.0 redirector.gvt1.com >> hosts
+echo redirector.gvt1.com
+echo. >> hosts
+echo 0.0.0.0 s0.2mdn.net >> hosts
+echo s0.2mdn.net
+echo. >> hosts
+echo 0.0.0.0 securepubads.g.doubleclick.net >> hosts
+echo securepubads.g.doubleclick.net
+echo. >> hosts
+echo 0.0.0.0 spclient.wg.spotify.com >> hosts
+echo spclient.wg.spotify.com
+echo. >> hosts
+echo 0.0.0.0 tpc.googlesyndication.com >> hosts
+echo tpc.googlesyndication.com
+echo.  >> hosts
+echo 0.0.0.0 v.jwpcdn.com >> hosts
+echo v.jwpcdn.com
+echo.  >> hosts
+echo 0.0.0.0 video-ad-stats.googlesyndication.com >> hosts
+echo video-ad-stats.googlesyndication.com
+echo.  >> hosts
+echo 0.0.0.0 weblb-wg.gslb.spotify.com >> hosts
+echo weblb-wg.gslb.spotify.com
+echo. >> hosts
+echo 0.0.0.0 www.googleadservices.com >> hosts
+echo www.googleadservices.com
+echo. >> hosts
+echo 0.0.0.0 www.omaze.com >> hosts
+echo www.omaze.com
+timeout /t 3
+echo  ________  ________  _____ ______   ________  ___       _______  _________  _______   ________         
+echo ^|\   ____\^|\   __  \^|\   _ \  _   \^|\   __  \^|\  \     ^|\  ___ \^|\___   ___\\  ___ \ ^|\   ___ \        
+echo \ \  \___^|\ \  \^|\  \ \  \\\__\ \  \ \  \^|\  \ \  \    \ \   __/\^|___ \  \_\ \   __/^|\ \  \_^|\ \       
+echo  \ \  \    \ \  \\\  \ \  \\^|__^| \  \ \   ____\ \  \    \ \  \_^|/__  \ \  \ \ \  \_^|/_\ \  \ \\ \      
+echo   \ \  \____\ \  \\\  \ \  \    \ \  \ \  \___^|\ \  \____\ \  \_^|\ \  \ \  \ \ \  \_^|\ \ \  \_\\ \ ___ 
+echo    \ \_______\ \_______\ \__\    \ \__\ \__\    \ \_______\ \_______\  \ \__\ \ \_______\ \_______\\__\
+echo     \^|_______^|\^|_______^|\^|__^|     \^|__^|\^|__^|     \^|_______^|\^|_______^|   \^|__^|  \^|_______^|\^|_______\^|__^|
+PAUSE
